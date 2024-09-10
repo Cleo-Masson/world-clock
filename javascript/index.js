3afeefba14a74552bb908aab83e6b7d2d3598187
Cleo-Masson <cleonikita50@gmail.com>
2024-09-10 16:20:56 +0400

    Added link to go back to homepage

--- javascript/index.js
+++ javascript/index.js
@@ -43,7 +43,8 @@ function UpdateSelectedCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )} <small>${cityTime.format("A")}</small></div>
-        </div>
+        </div> </br>
+<a href="/">All cities</a>
         
   `;
 }
